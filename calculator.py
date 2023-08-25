20d58a4deff54d6889f2240f9c2f47dfac1de086
Nwachukwu1300 <85280990+Nwachukwu1300@users.noreply.github.com>
2023-08-25 12:52:48 +0100

    used for loop to make buttons

--- calculator.py
+++ calculator.py
@@ -89,60 +89,41 @@ def button_divide():
         f_num = int(first_num)
     e.delete(0,END)
 
+# Define button labels and corresponding commands
+button_specs = [
+    ("7", lambda: button_click(7)),
+    ("8", lambda: button_click(8)),
+    ("9", lambda: button_click(9)),
+    ("4", lambda: button_click(4)),
+    ("5", lambda: button_click(5)),
+    ("6", lambda: button_click(6)),
+    ("1", lambda: button_click(1)),
+    ("2", lambda: button_click(2)),
+    ("3", lambda: button_click(3)),
+    ("0", lambda: button_click(0)),
+    ("+", button_add),
+    ("-", button_sub),
+    ("X", button_multiply),
+    ("/", button_divide),
+    ("=", button_equal),
+    ("CLEAR", button_clear),
+    ("⌫", button_back),
+    (".", button_dec)
+]
+
 root = Tk()
 root.title("Mmesoma's Calculator")
 
-e = Entry(root, width=40,borderwidth=6)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+e = Entry(root, width=40, borderwidth=6)
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-
-#define buttons
-button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_addition = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_subtraction = Button(root, text="-", padx=41, pady=20, command=button_sub)
-button_multiplication = Button(root, text="X", padx=40, pady=20, command=button_multiply)
-button_division = Button(root, text="/", padx=41, pady=20, command=button_divide)
-button_equalto = Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clearpage = Button(root, text="CLEAR", padx=79, pady=20, command=button_clear)
-button_backspace = Button(root, text="⌫", padx=40, pady=20, command=button_back)
-button_decimal = Button(root, text=".", padx=40, pady=20, command=button_dec)
-
-
-
-#put buttons on screen
-button_1.grid(row=2, column=0, padx=10, pady=10)
-button_2.grid(row=2, column=1, padx=10, pady=10)
-button_3.grid(row=2, column=2, padx=10, pady=10)
-
-button_4.grid(row=3, column=0, padx=10, pady=10)
-button_5.grid(row=3, column=1, padx=10, pady=10)
-button_6.grid(row=3, column=2, padx=10, pady=10)
-
-button_7.grid(row=4, column=0, padx=10, pady=10)
-button_8.grid(row=4, column=1, padx=10, pady=10)
-button_9.grid(row=4, column=2, padx=10, pady=10)
-
-button_0.grid(row=5, column=0, padx=10, pady=10)
-button_addition.grid(row=2, column=3, padx=10, pady=10)
-button_subtraction.grid(row=3, column=3, padx=10, pady=10)
-button_multiplication.grid(row=4, column=3, padx=10, pady=10)
-button_division.grid(row=5, column=3, padx=10, pady=10)
-
-button_clearpage.grid(row=5, column=1, padx=10, pady=10)
-button_backspace.grid(row=5, column=2, padx=10, pady=10)
-button_equalto.grid(row=6, column=0, columnspan=4, padx=10, pady=10)
-button_decimal.grid(row=6, column=1, padx=10, pady=10)
-
-
-
+# Create and place buttons using nested loops
+row_start = 2
+col_start = 0
+for i, (label, command) in enumerate(button_specs):
+    row = row_start + i // 4
+    col = col_start + i % 4
+    Button(root, text=label, padx=40, pady=20, command=command).grid(row=row, column=col, padx=10, pady=10)
 
 root.mainloop()
+
